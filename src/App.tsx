55cd2ccfcbe0238ade34c3b66452bc3b4007ade5
EvansAbraham <jea150702@outlook.com>
2025-09-13 20:41:02 +0530

    Final Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import gsap from "gsap"
-import { ScrollTrigger, SplitText } from "gsap/all"
+import gsap from "gsap";
+import Art from "./components/Art";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
+import Cocktails from "./components/Cocktails";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">
-        Hello, GSAP
-      </h1>
-    </div>
+    <main>
+        <NavBar/>
+        <Hero/>
+        <Cocktails/>
+        <About/>
+        <Art/>
+        <Menu/>
+        <Contact/>
+    </main>
   )
 }
 
